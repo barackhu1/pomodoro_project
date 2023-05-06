461e38961b673e3048bbdd2a9b3b3c17524e3977
barackhu1 <burnitdown01@gmail.com>
2023-05-06 13:56:42 +0200

    Starting project, help, with visszaszámláló.py -h, and play the sound with visszaszámláló.py -p

---
+++ visszaszámláló.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from playsound import playsound
+import sys
+
+HELP = """visszaszámláló v0.1 by BurnitDown (barackhu1@gmail.com), 2023
+
+Parameters:
+
+-h                      this help
+-p                      play the sound effect (for adjusting the volume)
+<time>                  where time can be given in different formats, e.g.
+                        60    -> 60 seconds
+                        60s   -> 60 seconds
+                        1m    -> 1 minute
+                        1m10s -> 1 minute and 10 seconds
+                        1h5m  -> 1 hour and 5 minutes"""
+
+
+def main():
+    try:
+        if sys.argv[1] == "-h":
+            print(HELP)
+        elif sys.argv[1] == "-p":
+            playsound("red sun in the sky.mp3")
+    except KeyboardInterrupt:
+        print("Kilépés")
+
+
+##############################################################################
+
+if __name__ == "__main__":
+    main()
